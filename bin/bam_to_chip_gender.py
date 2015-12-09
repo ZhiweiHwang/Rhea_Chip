080c6356961756cdb90cbb97d701db47fe1ee5fb
ZhiweiHwang <huang@Laboratory.local>
2015-12-09 23:04:50 +0800

    second commit

---
+++ bin/bam_to_chip_gender.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+get gender from bamfile
+"""
+
+from __future__ import absolute_import, unicode_literals, division
+import sys
+import os
+import pysam
+import re
+from toolz import partial, pipe
+from toolz.curried import map
+import numpy as np
+
+
+def average(sequence):
+	if not len(sequence):
+		return 0.0
+	try:
+		return sequence.mean()
+	except AttributeError:
+		return sum(sequence) / len(sequence)
+
+
+class Predict(object):
+	"""
+	gender prediction based on the depth
+	"""
+
+	def __init__(self, bamfile, outfile):
+		self.bamfile = bamfile
+		stat = self.indexbamfile()
+		self.outfile = outfile
+		assert self.bamfile and self.outfile and stat, "Input error"
+		self._bam = pysam.Samfile(bamfile)
+		self._prealloc_func = partial(np.zeros, dtype=np.int)
+		self.fake_bed_rows = [("chrX", 1, 59373566), ("chrY", 69362, 11375310)]
+		self.sequence = pipe(self.fake_bed_rows,
+		                     map(lambda interval: self.depthreader(*interval)),
+		                     map(average)
+		                     )
+		self.x_coverage, self.y_coverage = list(self.sequence)
+		self.sex = self.predict_gender()
+
+	def indexbamfile(self):
+		bamdir, bamfile = os.path.split(self.bamfile)
+		if os.path.isfile(self.bamfile + '.bai') or os.path.isfile(re.sub("bam$", 'bai', self.bamfile)):
+			return True
+		outdir = os.path.dirname(self.outfile)
+		if not os.access(bamdir, os.W_OK):
+			try:
+				os.symlink(self.bamfile, os.path.join(outdir, bamfile))
+				self.bamfile = os.path.join(outdir, bamfile)
+			except Exception:
+				return False
+		try:
+			pysam.index(self.bamfile)
+			return True
+		except Exception:
+			return False
+
+	def depthreader(self, contig, start, end):
+		pysam_start = start - 1
+		pysam_contig = str(contig)
+		if pysam_start < 0:
+			raise ValueError("Start position must be > 0, not %d" % start)
+		read_depths = self._prealloc_func(end - pysam_start)
+		for col in self._bam.pileup(pysam_contig, pysam_start, end, truncate=True):
+			read_depths[col.pos - pysam_start] = col.n
+		return read_depths
+
+	def predict_gender(self):
+		if self.x_coverage == 0:
+			return 'unknown'
+		elif (self.y_coverage > 0) and (self.x_coverage / self.y_coverage < 10):
+			return 'male'
+		else:
+			return 'female'
+
+
+def main():
+	usage = 'python %s <bamfile> <outfile>' % sys.argv[0]
+	if len(sys.argv) != 3:
+		return usage
+	bamfile = os.path.realpath(sys.argv[1])
+	outfile = os.path.realpath(sys.argv[2])
+	outdir, name = os.path.split(outfile)
+	out = open(outfile, 'w')
+	predict = Predict(bamfile, outdir)
+	out.writelines("\t".join([name, str(predict.sex), str(predict.x_coverage), str(predict.y_coverage)]) + '\n')
+	out.close()
+	return 0
+
+
+if __name__ == '__main__':
+	sys.exit(main())
---
+++ bin/vcf_phasing.py
@@ -0,0 +1,261 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Phasing the vcf file, add NB, PB
+"""
+
+import pysam
+import os
+import re
+import sys
+import gzip
+import itertools
+import tempfile
+import shutil
+
+
+def split_format(string1, string2):
+	fmat, val = string1.split(":"), string2.split(":")
+	val = val[:len(fmat)] if len(fmat) < len(val) else val + ['.'] * (len(fmat) - len(val))
+	return fmat, val
+
+
+def add_marker(string1, string2, key1, key2):
+	fmat, val = split_format(string1, string2)
+	string2 = ":".join(val)
+	if key1 in fmat:
+		return string1, string2
+	else:
+		return string1 + ':' + key1, string2 + ':' + key2
+
+
+def support_reads(bamfile, chrom, pos, ref, alt, gtype):
+	pybamfile = pysam.AlignmentFile(bamfile, 'rb')
+	pos = int(pos)
+	return_reads = {'first': list(), 'last': list()}
+	if len(ref) > len(alt):
+		ref = '-' * (len(ref) - len(alt))
+	g1, tag, g2 = re.compile('(\d)([/\|])(\d)').match(gtype).groups()
+	if g1 == 1:
+		refer = 'last'
+		mut = 'first'
+	else:
+		refer = 'first'
+		mut = 'last'
+	for pc in pybamfile.pileup(chrom, pos - 1, pos):
+		if pc.pos != pos - 1:
+			continue
+		for pr in pc.pileups:
+			align = pr.alignment
+			if pr.is_del or pr.indel < 0:
+				bases = '-' * abs(pr.indel)
+			else:
+				pos_start = pr.query_position
+				pos_end = pr.query_position + pr.indel + 1
+				bases = align.seq[pos_start:pos_end]
+			if bases == ref:
+				return_reads[refer].append(align.query_name)
+			elif bases == alt:
+				return_reads[mut].append(align.query_name)
+	return return_reads
+
+
+def reads_phasing(reads1, reads2):
+	compare1 = len([reads for reads in reads1['first'] if reads in reads2['first']])
+	compare2 = len([reads for reads in reads1['first'] if reads in reads2['last']])
+	compare3 = len([reads for reads in reads1['last'] if reads in reads2['first']])
+	compare4 = len([reads for reads in reads1['last'] if reads in reads2['last']])
+	if compare1 > compare2 and compare4 > compare3:
+		return 1
+	if compare2 > compare1 and compare3 > compare4:
+		return 2
+	return 0
+
+
+def readfile(infile, bamfile):
+	returnlist = list()
+	addformat = "##FORMAT=<ID=NB,Number=1,Type=String,Description=\"Neighbor Block\">\n" \
+	            "##FORMAT=<ID=PB,Number=1,Type=String,Description=\"Phasing Block\">\n"
+	f_in = gzip.open(infile, 'rb').readlines() if infile.endswith('.gz') else open(infile, 'rb').readlines()
+	vcfdict = {k: list(g) for k, g in itertools.groupby(f_in, lambda x: x.startswith('#'))}
+	header, variantinfo = vcfdict[True], [line.strip().split('\t') for line in vcfdict[False]]
+	formatline = max([num for num, line in enumerate(header) if line.startswith('##FORMAT')])
+	header.insert(formatline + 1, addformat)
+	nbnumber = {str(i): 0 for i in range(1, 26)}
+	pbnumber = {str(i): 0 for i in range(1, 26)}
+	pre_type, rev, pre_haplo = None, 0, dict()
+	for num, rows in enumerate(variantinfo[1:], 1):
+		if len(rows) != 10:
+			continue
+		pos = int(rows[1]) + len(rows[3]) - 1
+		chrom = rows[0].lower().replace('chr', '').replace('x', '23').replace('y', '24')
+		chrom = re.sub(".*m.*", "25", chrom)
+		mut = min(rows[4].split(","), key=len)
+		ref = rows[3]
+		if rows[0].lower() != variantinfo[num - 1][0].lower():
+			pre_type, rev, pre_haplo = None, 0, dict()
+		muttypes = 'SNP' if len(ref) == 1 and len(mut) == 1 else 'INDEL'
+		formats, values = split_format(rows[-2], rows[-1])
+		preformat, prevalue = split_format(variantinfo[num - 1][-2], variantinfo[num - 1][-1])
+		formatdict = dict(zip(formats, values))
+		preformatdict = dict(zip(preformat, prevalue))
+		gtype = re.compile("(\d)([/\|])(\d)").match(formatdict['GT'])
+		pregtype = re.compile("(\d)([/\|])(\d)").match(preformatdict['GT'])
+		if gtype and (rows[6] == 'PASS' or rows[6] == '.'):
+			g1, tag, g2 = gtype.groups()
+			if g1 == g2:
+				formatdict['GT'] = str(g1) + '|' + str(g2)
+			elif 'HP' in formatdict:
+				if 'HP' in preformatdict:
+					prehaplo = re.split(',|-', preformatdict['HP'])
+					preg1, pretag, preg2 = pregtype.groups()
+					if prehaplo[0] not in pre_haplo:
+						pre_haplo[prehaplo[0]] = dict()
+					if prehaplo[1] not in pre_haplo[prehaplo[0]]:
+						pre_haplo[prehaplo[0]][prehaplo[1]] = preg1
+					if prehaplo[2] not in pre_haplo:
+						pre_haplo[prehaplo[2]] = dict()
+					if prehaplo[3] not in pre_haplo[prehaplo[2]]:
+						pre_haplo[prehaplo[2]][prehaplo[3]] = preg2
+				haplo = re.split(',|-', formatdict['HP'])
+				if haplo[0] in pre_haplo and haplo[1] in pre_haplo[haplo[0]] and haplo[3] in pre_haplo[haplo[0]]:
+					formatdict['GT'] = str(pre_haplo[haplo[0]][haplo[1]]) + '|' + str(pre_haplo[haplo[0]][haplo[3]])
+					if ':PB' not in variantinfo[num - 1][-2]:
+						pbnumber[chrom] += 1
+					variantinfo[num][-2], variantinfo[num][-1] = add_marker(variantinfo[num][-2],
+					                                                        variantinfo[num][-1],
+					                                                        'PB',
+					                                                        "P{:0>2d}{:0>5d}".format(
+						                                                        int(chrom), int(pbnumber[chrom])))
+					variantinfo[num - 1][-2], variantinfo[num - 1][-1] = add_marker(variantinfo[num - 1][-2],
+					                                                                variantinfo[num - 1][-1],
+					                                                                'PB',
+					                                                                "P{:0>2d}{:0>5d}".format(
+						                                                                int(chrom),
+						                                                                int(pbnumber[chrom])))
+				else:
+					pre_haplo[haplo[0]] = dict()
+					pre_haplo[haplo[0]][haplo[1]] = str(g1)
+					pre_haplo[haplo[0]][haplo[3]] = str(g2)
+			elif muttypes == 'SNP':
+				if pre_type == 'SNP':
+					if g1 != g2:
+						if tag == '|':
+							if ':PB' not in variantinfo[num - 1][-2]:
+								pbnumber[chrom] += 1
+							variantinfo[num][-2], variantinfo[num][-1] = add_marker(variantinfo[num][-2],
+							                                                        variantinfo[num][-1],
+							                                                        'PB',
+							                                                        "P{:0>2d}{:0>5d}".format(
+								                                                        int(chrom),
+								                                                        int(pbnumber[chrom])))
+							variantinfo[num - 1][-2], variantinfo[num - 1][-1] = add_marker(variantinfo[num - 1][-2],
+							                                                                variantinfo[num - 1][-1],
+							                                                                'PB',
+							                                                                "P{:0>2d}{:0>5d}".format(
+								                                                                int(chrom),
+								                                                                int(pbnumber[chrom])))
+							if rev:
+								formatdict['GT'] = str(g2) + '|' + str(g1)
+						else:
+							rev = 0
+				elif pre_type == 'INDEL':
+					if g1 != g2:
+						pre_reads = support_reads(bamfile, variantinfo[num - 1][0], variantinfo[num - 1][1],
+						                          variantinfo[num - 1][3], variantinfo[num - 1][4], preformatdict['GT'])
+						now_reads = support_reads(bamfile, variantinfo[num][0], variantinfo[num][1],
+						                          variantinfo[num][3], variantinfo[num][4], formatdict['GT'])
+						phasing = reads_phasing(pre_reads, now_reads)
+						if phasing:
+							if ':PB' not in variantinfo[num - 1][-2]:
+								pbnumber[chrom] += 1
+							variantinfo[num][-2], variantinfo[num][-1] = add_marker(variantinfo[num][-2],
+							                                                        variantinfo[num][-1], 'PB',
+							                                                        "P{:0>2d}{:0>5d}".format(
+								                                                        int(chrom),
+							                                                            int(pbnumber[chrom])))
+							variantinfo[num - 1][-2], variantinfo[num - 1][-1] = add_marker(variantinfo[num - 1][-2],
+							                                                                variantinfo[num - 1][-1],
+							                                                                'PB',
+							                                                                "P{:0>2d}{:0>5d}".format(
+								                                                                int(chrom),
+								                                                                int(pbnumber[chrom])))
+							if phasing == 1:
+								rev = 0 if rev == 0 else 1
+							else:
+								rev = 1 if rev == 0 else 0
+							formatdict['GT'] = str(g1) + '|' + str(g2) if rev == 0 else str(g2) + '|' + str(g1)
+						else:
+							rev = 0
+			else:
+				if g1 != g2:
+					pre_reads = support_reads(bamfile, variantinfo[num - 1][0], variantinfo[num - 1][1],
+					                          variantinfo[num - 1][3], variantinfo[num - 1][4], preformatdict['GT'])
+					now_reads = support_reads(bamfile, variantinfo[num][0], variantinfo[num][1],
+					                          variantinfo[num][3], variantinfo[num][4], formatdict['GT'])
+					phasing = reads_phasing(pre_reads, now_reads)
+					if phasing:
+						if ':PB' not in variantinfo[num - 1][-2]:
+							pbnumber[chrom] += 1
+						variantinfo[num][-2], variantinfo[num][-1] = add_marker(variantinfo[num][-2],
+						                                                        variantinfo[num][-1], 'PB',
+						                                                        "P{:0>2d}{:0>5d}".format(int(chrom),
+						                                                                                 int(pbnumber[
+							                                                                                 chrom])))
+						variantinfo[num - 1][-2], variantinfo[num - 1][-1] = add_marker(variantinfo[num - 1][-2],
+						                                                                variantinfo[num - 1][-1], 'PB',
+						                                                                "P{:0>2d}{:0>5d}".format(
+							                                                                int(chrom),
+							                                                                int(pbnumber[chrom])))
+						if phasing == 1:
+							rev = 0 if rev == 0 else 1
+						else:
+							rev = 1 if rev == 0 else 0
+						formatdict['GT'] = str(g1) + '|' + str(g2) if rev == 0 else str(g2) + tag + str(g1)
+					else:
+						rev = 0
+			pre_type = muttypes
+
+		if rows[0] == variantinfo[num - 1][0] and \
+			pos - (int(variantinfo[num - 1][1]) + len(variantinfo[num - 1][3]) - 1) <= 5:
+			if ':NB' not in variantinfo[num - 1][-2]:
+				nbnumber[chrom] += 1
+			variantinfo[num][-2], variantinfo[num][-1] = add_marker(variantinfo[num][-2], variantinfo[num][-1], 'NB',
+			                                                        "N{:0>2d}{:0>5d}".format(int(chrom),
+			                                                                                 int(nbnumber[chrom])))
+			variantinfo[num - 1][-2], variantinfo[num - 1][-1] = add_marker(variantinfo[num - 1][-2],
+			                                                                variantinfo[num - 1][-1], 'NB',
+			                                                                "N{:0>2d}{:0>5d}".format(
+				                                                                int(chrom), int(nbnumber[chrom])))
+		formatlast, valuelast = split_format(variantinfo[num][-2], variantinfo[num][-1])
+		for i in range(len(formatlast)):
+			if formatlast[i] in formatdict:
+				valuelast[i] = formatdict[formatlast[i]]
+		variantinfo[num][-2], variantinfo[num][-1] = ":".join(formatlast), ":".join(valuelast)
+	returnlist.extend(header)
+	returnlist.extend(["\t".join(i) + '\n' for i in variantinfo])
+	return returnlist
+
+
+def main():
+	usage = 'python %s <bamfile> <VcfIn> <VcfOut>' % sys.argv[0]
+	if len(sys.argv) != 4:
+		return usage
+	bamfile = os.path.realpath(sys.argv[1])
+	infile = os.path.realpath(sys.argv[2])
+	outfile = os.path.realpath(sys.argv[3])
+	temppath = tempfile.mkdtemp()
+	resultfileout = os.path.join(temppath, os.path.basename(outfile))
+	f_out = open(re.sub("\.gz$", "", resultfileout), 'w')
+	f_out.writelines(readfile(infile, bamfile))
+	f_out.close()
+	if resultfileout.endswith('.gz'):
+		try:
+			resultfileout = pysam.tabix_index(resultfileout, preset='vcf')
+		except Exception:
+			pass
+	shutil.copy(resultfileout, outfile)
+
+if __name__ == '__main__':
+	sys.exit(main())
